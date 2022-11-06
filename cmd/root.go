028e1bf8cabdbfb1ea58a8c14389275d099da182
jon4hz <me@jon4hz.io>
2022-11-06 17:51:26 +0100

    fix: tls config file

--- cmd/root.go
+++ cmd/root.go
@@ -66,7 +66,7 @@ func root(cmd *cobra.Command, args []string) {
 	})
 
 	server := &http.Server{Addr: cfg.Listen, ReadHeaderTimeout: 5 * time.Second}
-	if err := web.ListenAndServe(server, cfg.TLSConfigFile, logger); err != nil {
+	if err := web.ListenAndServe(server, &web.FlagConfig{WebConfigFile: &cfg.TLSConfigFile}, logger); err != nil {
 		level.Error(logger).Log("msg", "Failed to start the server", "err", err) // nolint: errcheck
 		os.Exit(1)
 	}
